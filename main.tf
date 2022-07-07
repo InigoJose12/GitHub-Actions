509014f78d146f004d72d32b11c101e0db37b2d2
InigoJose12 <103239271+InigoJose12@users.noreply.github.com>
2022-07-07 20:20:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,14 +2,13 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.26.0"
+      version = "~> 3.50.0"
     }
     random = {
       source  = "hashicorp/random"
       version = "3.0.1"
     }
   }
-  required_version = ">= 1.1.0"
 
   cloud {
     organization = "REPLACE_ME"
