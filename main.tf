56c047a7a0009c4310f12bf72e7ec8805869d853
InigoJose12 <103239271+InigoJose12@users.noreply.github.com>
2022-07-09 19:25:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,10 +4,6 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 3.50.0"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.0.1"
-    }
   }
   cloud {
     organization = "terraform-organisation-1997"
